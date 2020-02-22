97542ffe98522a879234f5c29ad6a6dc03e28a15
Robert Grizzard <grizzardrl@vcu.edu>
2020-02-22 10:50:09 -0500

    Initial commit

---
+++ macos-temperature-monitor/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  macos-temperature-monitor
+//
+//  Created by Robert Grizzard on 2/22/20.
+//  Copyright © 2020 Robert Grizzard. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
